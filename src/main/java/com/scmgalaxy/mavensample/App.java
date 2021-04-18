af9c32650cdff17511c790a9ca6c915c605ce7f1
Udhay7 <udhay.prakash@gmail.com>
2021-04-18 12:13:56 +1000

    Update App.java

--- src/main/java/com/scmgalaxy/mavensample/App.java
+++ src/main/java/com/scmgalaxy/mavensample/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World RefX! ALP-2--Wp" );
+        System.out.println( "Hello World!" );
     }
 }
